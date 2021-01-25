e12794fe023208dd96fece0c3b7c331760bb6a1e
Frank Nolasco Velasquez <49081234+FranksitoKawai@users.noreply.github.com>
2021-01-25 00:48:21 -0500

    sql scripts

--- src/functions/SqlScripts.js
+++ src/functions/SqlScripts.js
@@ -1,21 +1,12 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
+  // user: "root",
   user: "frank",
+  // password: "Frank09por123*",
   password: "Frank09p0r123*",
   database: "acabados",
 });
-
-function executeQuery(query, res) {
-  if (!connection._connectCalled) {
-    connection.connect();
-  }
-  connection.query(query, (err, rows, fields) => {
-    if (err) throw err;
-    connection.end();
-    res.send(rows);
-  });
-}
 
 function callProcedure(procedure, res,next) {
   if (!connection._connectCalled) {
@@ -27,9 +18,20 @@ function callProcedure(procedure, res,next) {
   });
 }
 
+function callProcedureCallback(procedure, cb ) {
+  if (!connection._connectCalled) {
+    connection.connect();
+  }
+  connection.query(`CALL ${procedure}`, (err, rows, fields) => {
+    if (err) return next(err);
+    cb(rows)
+  });
+}
+
+
 const Scripts = {
-  executeQuery,
   callProcedure,
+  callProcedureCallback,
   connection,
 };
 
