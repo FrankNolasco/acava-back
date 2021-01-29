7f9fc27c1643b3ce8bebc691065414060d1221b8
Frank Nolasco Velasquez <49081234+FranksitoKawai@users.noreply.github.com>
2021-01-28 22:04:45 -0500

    Solucion 1

--- src/controllers/SecurityController.js
+++ src/controllers/SecurityController.js
@@ -3,36 +3,44 @@ const jwt = require("jsonwebtoken");
 const { validarRows , encontrarCabeceras } = require("../functions/utils");
 
 const VerificarRol = ( req, res, next ) => {
-    const headers = encontrarCabeceras(req)
-    if(headers){
-        callProcedureCallback(`consultarClaveSuperSecretaRol('${headers.rol}')`,(rows) => {
-            if(validarRows(rows)){
-                const supersecret = rows[0][0].ClaveSupersecreta
-                jwt.verify(headers.token, supersecret ,(err) => {
-                    if (!err) return next();
-                });
-            }
-        })
+    try {
+        const headers = encontrarCabeceras(req)
+        if(headers){
+            callProcedureCallback(`consultarClaveSuperSecretaRol('${headers.rol}')`,(rows) => {
+                if(validarRows(rows)){
+                    const supersecret = rows[0][0].ClaveSupersecreta
+                    jwt.verify(headers.token, supersecret ,(err) => {
+                        if (!err) return next();
+                        else throw new Error()
+                    });
+                }else{
+                    throw new Error()
+                }
+            })
+        }else{
+            throw new Error()
+        }
+    } catch (error) {
+        res.sendStatus(502)
     }
-    console.log("mira como me ejecuto crack")
-    res.sendStatus(404)
 }
 
 const verificarModulo = (req, res, next) => {
-    let centinela = false
-    const headers = encontrarCabeceras(req);
-    const { idModulo } = req.body
-    if (headers) {
-        callProcedureCallback(`consultarModuloPorRol('${idModulo}','${headers.rol}')` , (rows) => {
-            console.log("validar rows2",validarRows(rows))
-            if(validarRows(rows)) centinela = true
-        })
+    try {
+        const headers = encontrarCabeceras(req);
+        const { idModulo } = req.body
+        if (headers) {
+            callProcedureCallback(`consultarModuloPorRol('${idModulo}','${headers.rol}')` , (rows) => {
+                console.log("validar rows2",validarRows(rows))
+                if(validarRows(rows)) next()
+                else throw new Error()
+            })
+        }
+        else throw new Error()
+    } catch (error) {
+        res.sendStatus(502)
     }
-    if(centinela){
-        return next()
-    }else{
-        res.sendStatus(500)
-    }
+    
 }
 
 const verificarPermiso = (req, res, next) => {
