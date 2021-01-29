2d13e69ccb68df3d67a85fb396d7fb85f9217001
Frank Nolasco Velasquez <49081234+FranksitoKawai@users.noreply.github.com>
2021-01-28 21:22:56 -0500

    console.log de prueba?

--- src/controllers/SecurityController.js
+++ src/controllers/SecurityController.js
@@ -5,11 +5,16 @@ const { validarRows , encontrarCabeceras } = require("../functions/utils");
 const VerificarRol = ( req, res, next ) => {
     let centinela = false
     const headers = encontrarCabeceras(req)
+    console.log(headers)
     if(headers){
         callProcedureCallback(`consultarClaveSuperSecretaRol('${headers.rol}')`,(rows) => {
+            console.log(rows)
+            console.log("validar rows",validarRows(rows))
             if(validarRows(rows)){
                 const supersecret = rows[0][0].ClaveSupersecreta
+                console.log(supersecret)
                 jwt.verify(headers.token, supersecret , function (err) {
+                    console.log(err)
                     if (!err) centinela = true;
                 });
             }
@@ -26,9 +31,13 @@ const VerificarRol = ( req, res, next ) => {
 const verificarModulo = (req, res, next) => {
     let centinela = false
     const headers = encontrarCabeceras(req);
+    console.log(headers)
     const { idModulo } = req.body
+    console.log(req.body)
     if (headers) {
         callProcedureCallback(`consultarModuloPorRol('${idModulo}','${headers.rol}')` , (rows) => {
+            console.log(rows)
+            console.log("validar rows2",validarRows(rows))
             if(validarRows(rows)) centinela = true
         })
     }
