24a4a485e026765364e094d985aec27b167bf90f
Frank Nolasco Velasquez <49081234+FranksitoKawai@users.noreply.github.com>
2021-02-20 03:22:57 -0500

    ssl added

--- src/security/SSL.js
+++ src/security/SSL.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
-const ca = fs.readFileSync('./www_acavadosserviciosgenerales_com.ca-bundle')
-const crt = fs.readFileSync('./www_acavadosserviciosgenerales_com.crt')
-const key = fs.readFileSync('./acavadosserviciosgenerales_com.key')
+const path = require('path')
+const ca = fs.readFileSync(path.join(__dirname,'/www_acavadosserviciosgenerales_com.ca-bundle'))
+const crt = fs.readFileSync(path.join(__dirname,'/www_acavadosserviciosgenerales_com.crt'))
+const key = fs.readFileSync(path.join(__dirname,'/acavadosserviciosgenerales_com.key'))
 
 const SSL = {
  crt,ca,key
